9e4215a1055ec0467b19e0837b92fc687833ff5a
Ashim Neupane <asimneupane50@gmail.com>
2025-11-26 20:32:02 +0545

    deploy 01

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -10,7 +10,7 @@
     <meta name="robots" content="index, follow">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/fav.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fonts/lineicons/font-css/LineIcons.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
